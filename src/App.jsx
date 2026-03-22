3be00c1fa0bd6c7aafc82c219f801638d4560381
Mildred Nwonye <nwonyemildred@gmail.com>
2026-03-22 18:28:11 +0100

    feat: Replace placeholder `OrderList` with `RestaurantOwnerOrders` component and update the orders route path.

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const AppRoutes = () => {
             } 
           />
           <Route 
-            path="/orders/" 
+            path="/orders" 
             element={
               <ProtectedRoute>
                 {user?.role === 'restaurant_owner' 
